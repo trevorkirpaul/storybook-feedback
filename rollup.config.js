a722e045ee60edc7f4856d7394a10c6cd0b7dcd5
Trevor Kirpaul <trevorkirpaul@gmail.com>
2019-06-20 23:41:09 -0400

    implemented minifier and updated gitignore

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import rollupTypescript from 'rollup-plugin-typescript'
+import minify from 'rollup-plugin-babel-minify'
 
 export default {
   input: 'src/main.tsx',
@@ -15,6 +16,9 @@ export default {
     babel({
       exclude: 'node_modules/**', // only transpile our source code
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    minify({
+      comments: false, // don't preserve comments
     }),
   ],
   external: ['react', '@storybook/addons', 'styled-components'],
